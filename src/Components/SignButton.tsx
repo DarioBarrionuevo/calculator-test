81ead5061adeff3f59ea3cc27a125c1b729a9847
Dario Barrionuevo <dariobafran@gmail.com>
2023-10-03 19:12:41 +0200

    Create SignButton component

---
+++ src/Components/SignButton.tsx
@@ -0,0 +1,33 @@
+import { Button, Grid, styled } from "@mui/material";
+
+interface SignButtonProps {
+  operation: string;
+  pickOperation: (operation: string) => void;
+  selectedOperation: string;
+}
+
+// Styled components
+const StyledButton = styled(Button)<{ selected: boolean }>((props) => ({
+  backgroundColor:"#2f589c",
+  borderColor: props.selected ? "#fff" : "#5493f7",
+}));
+
+const SignButton: React.FC<SignButtonProps> = ({
+  operation,
+  pickOperation,
+  selectedOperation,
+}) => {
+  return (
+    <Grid item>
+      <StyledButton
+        fullWidth
+        variant="outlined"
+        onClick={() => pickOperation(operation)}
+        selected={selectedOperation === operation}
+      >
+        {operation}
+      </StyledButton>
+    </Grid>
+  );
+};
+export default SignButton;
