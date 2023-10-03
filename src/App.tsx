2dfd8bd076ca31e8962057466286a667fd3db472
Dario Barrionuevo <dariobafran@gmail.com>
2023-10-03 21:29:23 +0200

    Fix equal button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Container, Grid, Paper, styled } from "@mui/material";
+import { Container, Grid, Paper, styled, Button } from "@mui/material";
 import { useState } from "react";
 import SignButton from "./Components/SignButton";
 import NumberButton from "./Components/NumberButton";
 
 // Styled components
 const Calculator = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(1),
+  padding: theme.spacing(3),
   marginTop: theme.spacing(10),
   borderRadius: 10,
 }));
@@ -99,11 +99,11 @@ function App() {
           <Grid item container columnSpacing={1}>
             <NumberButton xs={6} digit={"0"} enterDigit={pickNumber} />
             <NumberButton digit={"."} enterDigit={() => {}} />
-            <SignButton
-              operation={"="}
-              pickOperation={pickOperation}
-              selectedOperation={operation}
-            />
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                =
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
       </Calculator>
