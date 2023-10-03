f133b0774fb25a0e93b9cff43bc73504ac1ec8af
Dario Barrionuevo <dariobafran@gmail.com>
2023-10-03 18:36:25 +0200

    Added Display StyledComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import { Container, Paper, styled } from "@mui/material";
+import { Container, Grid, Paper, styled } from "@mui/material";
 
+// Styled components
 const Calculator = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(3),
+  padding: theme.spacing(1),
   marginTop: theme.spacing(10),
   borderRadius: 10,
 }));
+const Display = styled(`div`)(({ theme }) => ({
+  width: "100%",
+  textAlign: "right",
+  height: "2em",
+  padding: theme.spacing(2),
+  fontSize: "5em",
+  overflow: "hidden",
+}));
+
 function App() {
   return (
     <Container>
-      <Calculator elevation={6}>Testing Material-UI</Calculator>
+      <Calculator elevation={6}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Display>Test Material-UI</Display>
+          </Grid>
+        </Grid>
+      </Calculator>
     </Container>
   );
 }
