87189ca1233a295c93857ef3320f6d7e47fd0997
Dario Barrionuevo <dariobafran@gmail.com>
2023-10-03 21:23:48 +0200

    Complete  buttons layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Container, Grid, Paper, styled } from "@mui/material";
 import { useState } from "react";
 import SignButton from "./Components/SignButton";
+import NumberButton from "./Components/NumberButton";
 
 // Styled components
 const Calculator = styled(Paper)(({ theme }) => ({
@@ -34,10 +35,12 @@ function App() {
     <Container>
       <Calculator elevation={6}>
         <Grid container spacing={1}>
+          {/* Display */}
           <Grid item xs={12}>
             <Display>{currentValue}</Display>
           </Grid>
-          <Grid item container columnSpacing={5}>
+          {/* First row */}
+          <Grid item container columnSpacing={1}>
             <SignButton
               operation={"AC"}
               pickOperation={pickOperation}
@@ -55,6 +58,49 @@ function App() {
             />
             <SignButton
               operation={"÷"}
+              pickOperation={pickOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          {/* Second row */}
+          <Grid item container columnSpacing={1}>
+            <NumberButton digit={"7"} enterDigit={pickNumber} />
+            <NumberButton digit={"8"} enterDigit={pickNumber} />
+            <NumberButton digit={"9"} enterDigit={pickNumber} />
+            <SignButton
+              operation={"x"}
+              pickOperation={pickOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          {/* Third row */}
+          <Grid item container columnSpacing={1}>
+            <NumberButton digit={"4"} enterDigit={pickNumber} />
+            <NumberButton digit={"5"} enterDigit={pickNumber} />
+            <NumberButton digit={"6"} enterDigit={pickNumber} />
+            <SignButton
+              operation={"-"}
+              pickOperation={pickOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          {/* Fourth row */}
+          <Grid item container columnSpacing={1}>
+            <NumberButton digit={"1"} enterDigit={pickNumber} />
+            <NumberButton digit={"2"} enterDigit={pickNumber} />
+            <NumberButton digit={"3"} enterDigit={pickNumber} />
+            <SignButton
+              operation={"+"}
+              pickOperation={pickOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          {/* Fifth row */}
+          <Grid item container columnSpacing={1}>
+            <NumberButton xs={6} digit={"0"} enterDigit={pickNumber} />
+            <NumberButton digit={"."} enterDigit={() => {}} />
+            <SignButton
+              operation={"="}
               pickOperation={pickOperation}
               selectedOperation={operation}
             />
